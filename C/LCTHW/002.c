66570951ae2b2183c83208e34a263511b8dab0f3
PalDew <palashofbackup@gmail.com>
2024-04-04 19:35:33 +0530

    exercise 4

---
+++ C/LCTHW/002.c
@@ -0,0 +1,14 @@
+/* In this program, we output something*/
+
+#include<stdio.h>
+
+int main()
+{
+    int age =10;
+    int height=110;
+
+    printf("I am %d years old\n", age);
+    printf("I am %d inches tall\n", height);
+
+    return 0;
+}
