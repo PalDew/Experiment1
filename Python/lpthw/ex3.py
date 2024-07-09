81d7dbaf4d9c07bc2c4985aa49b94474899781cd
PalDew <160616228+PalDew@users.noreply.github.com>
2024-07-09 13:12:08 +0530

    ex3 homework

--- Python/lpthw/ex3.py
+++ Python/lpthw/ex3.py
@@ -5,7 +5,7 @@ print("Roosters", 100 -25 *3 % 4)
 
 print("Now I will count the eggs:")
 
-print(3+2+1-5+4%2-1/+6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 /4 +6)
 
 print("Is it true that 3+2<5-7?")
 
@@ -16,7 +16,7 @@ print("What is 5-7:", 5-7)
 
 print("Oh that's why it is false!")
 
-print("How about some`: more?")
+print("How about some: more?")
 
 print("Is is greater?", 5>-2)
 print("Is it greaater or equal?",5 >= -2)
---
+++ Python/lpthw/ex3hw.py
@@ -0,0 +1,30 @@
+print("I will now count my Chickens:")
+
+print("Hens",25.0 + 30.0 /6.0)
+print("Roosters", 100.0 -25.0 *3.0 % 4.0)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 /4 +6)
+
+print("Is it true that 3+2<5-7?")
+
+print(3.0+2.0<5.0-7.0)
+
+print("What is 3+2:",3.0+2.0)
+print("What is 5-7:", 5.0-7.0)
+
+print("Oh that's why it is false!")
+
+print("How about some more?")
+
+print("Is is greater?", 5.0>-2.0)
+print("Is it greaater or equal?",5.0 >= -2.0)
+print("Is it less or equal",5.0 <= -2.0)
+
+print("Testing the Modulo operator now:")
+print("What is 7 % 2? ", 7 % 2)
+print("What is 7.0 % 2.0?", 7.0 % 2.0)
+print("What is 4.0 % 2?", 4.0 % 2)
+print("What is 7.0 / 2?", 7.0/2)
+print("5 % 2.5", 5 % 2.5)
