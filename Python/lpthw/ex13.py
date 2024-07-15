54cab0380ab4be9931bd6b85a85123c3ff50ec13
PalDew <160616228+PalDew@users.noreply.github.com>
2024-07-15 12:15:09 +0530

    fffff

--- Python/lpthw/ex13.py
+++ Python/lpthw/ex13.py
@@ -5,6 +5,8 @@ script, first, second, third = argv
 print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
-print("Your thrid variaable is:", third)
+print("Your thrid variable is:", third)
+
+
 
 
---
+++ Python/lpthw/ex13hw.py
@@ -0,0 +1,14 @@
+from sys import argv
+# read the WYSS section for how to run this
+a, b, = argv
+
+print("The script is called:", a)
+print("Your first variable is:", b)
+c = input("Enter second variable ")
+d = input("Enter third variable ")
+print("Your second variable is: ",c)
+print("Your thrid variable is: ", d)
+
+
+
+
