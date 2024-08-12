e24d4f00619a0b99a3df4d4a58bfee5640596031
PalDew <palashofbackup@gmail.com>
2024-08-12 17:28:05 +0530

    ex 14 and ex14.py created

--- Python/lpthw/ex11.py
+++ Python/lpthw/ex11.py
@@ -8,6 +8,7 @@ print(f"So, you are {age} years old, {height} inches tall and {weight} lbs heavy
 
 name = input ("What should I call you? ")
 print("What a pleasure", name, ", it is to meet you")
+print(f"What a pleasure {name}, it is to meet you!") 
 
 total = int(age) + int(height) + int (weight)
 print("The total of your numbers is", total, " maybe it is your lucky number.")
