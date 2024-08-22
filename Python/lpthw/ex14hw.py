03d266421341e1c2349b40729f98f00b3c064680
PalDew <palashofbackup@gmail.com>
2024-08-22 13:06:25 +0530

    Updating hte python files

--- Python/lpthw/ex14hw.py
+++ Python/lpthw/ex14hw.py
@@ -1,12 +1,12 @@
 from sys import argv
 
 script, user_name =  argv
-# prompt = '> '
+prompt = '> '
 
 print(f"Hi {user_name}, I am the {script} script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
-likes = input()
+likes = input() 
 
 print(f"Where do you live {user_name}?")
 lives = input()
