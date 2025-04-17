87f0c6b5a017485d6834d73e234b64c74e87f3c5
PalDew <palashofbackup@gmail.com>
2025-04-17 18:39:41 +0530

    just one half of a program
    erq

---
+++ C/LCTHW/007.c
@@ -0,0 +1,10 @@
+// Exercise 12 in the book
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int areas[] = {10, 12, 13, 14, 20};
+  char name[] = "Pal";
+  char full_name[] = {'P', 'a', 'l', 'a', 's', 'h', ' ', 'D', 'e', 'w', '\0'};
+
+  return 0;
+}
