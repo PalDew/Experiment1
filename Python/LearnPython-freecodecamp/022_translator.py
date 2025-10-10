d4f3638021b3be945258cac2f43e7b86ce1469d4
pal <pal@laptop>
2025-10-10 16:56:05 +0530

    from nix laptop

--- Python/LearnPython-freecodecamp/022_translator.py
+++ Python/LearnPython-freecodecamp/022_translator.py
@@ -6,10 +6,15 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
+        if letter in "AEIOU":
+            translation = translation + "G"
+        if letter in "aeiou": # alternatively, we can use: if letter.lower() in "aeiou"
             translation = translation + "g"
         else:
             translation = translation + letter
     return translation
 
 print(translate(input("Enter a phrase: ")))
+"""
+This is a multi-line comment.
+"""
---
+++ Python/LearnPython-freecodecamp/023_tryExcept.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+## This is aptly named section - catching errors in the program.
+
+number = int(input("Enter a number: "))
+print(number)
+#
+# This code fails when we enter a string, instead of a number.
+# invalid literal for for int() in base 10
